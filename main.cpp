caddf4e50cbb23c4c7641e8d192244f2cbeec202
“lothron” <yuewuxd@gmail.com>
2024-01-01 21:51:04 +0800

    -

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void printMenu() {
+  // 1 print help
+  std::cout << "1: Print help " << std::endl;
+  // 2 print exchange stats
+  std::cout << "2: Print exchange stats" << std::endl;
+  // 3 make an offer
+  std::cout << "3: Make an offer " << std::endl;
+  // 4 make a bid
+  std::cout << "4: Make a bid " << std::endl;
+  // 5 print wallet
+  std::cout << "5: Print wallet " << std::endl;
+  // 6 continue
+  std::cout << "6: Continue " << std::endl;
+
+  std::cout << "============== " << std::endl;
+
+// std::cout << "Current time is: " << currentTime << std::endl;
+}
+
+int main() {
+
+
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
